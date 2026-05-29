6efa02d55ac3e3d50e01eddaff86738b6773a479
Miguel Celen <miguel.celen@gmail.com>
2026-05-29 13:24:02 +0200

    navigatie-skelet met stack navigator

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import HomeScreen from "./screens/HomeScreen";
+import ProductDetailsScreen from "./screens/ProductDetailsScreen";
+import NewsDetailsScreen from "./screens/NewsDetailsScreen";
+import CampusDetailsScreen from "./screens/CampusDetailsScreen";
+import StudiezoekerScreen from "./screens/StudiezoekerScreen";
+import GameScreen from "./screens/GameScreen";
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: { backgroundColor: "#88bc25" },
+          headerTintColor: "#fff",
+          headerTitleStyle: { fontWeight: "bold" },
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Busleyden Atheneum" }}
+        />
+
+        <Stack.Screen
+          name="ProductDetails"
+          component={ProductDetailsScreen}
+          options={{ title: "Product" }}
+        />
+
+        <Stack.Screen
+          name="NewsDetails"
+          component={NewsDetailsScreen}
+          options={{ title: "Nieuws" }}
+        />
+
+        <Stack.Screen
+          name="CampusDetails"
+          component={CampusDetailsScreen}
+          options={{ title: "Campus" }}
+        />
+
+        <Stack.Screen
+          name="Studiezoeker"
+          component={StudiezoekerScreen}
+          options={{ title: "Studiezoeker" }}
+        />
+        
+        <Stack.Screen
+          name="Game"
+          component={GameScreen}
+          options={{ title: "BA Boekenjager" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
